9db7bec85a52ab1c7d5d95da94b3e234f59c4e62
psph0785 <psph0785@localhost>
2015-02-12 23:25:35 +0100

    Initialisation sping mvc

---
+++ rokmeul/src/main/java/rokmeul/com/controler/FrontController.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package rokmeul.com.controler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+/**
+ * @author root
+ *
+ */
+public class FrontController extends AbstractController {
+
+	@Override
+	protected ModelAndView handleRequestInternal(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+
+		ModelAndView modelandview = new ModelAndView("index");
+		modelandview.addObject("welcome message", "Hello and welcoem to rokmel with Spring MVC Framework !");
+		
+		return modelandview;
+	}
+
+}
